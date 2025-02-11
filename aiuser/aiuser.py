60c398f3cf80b6f4395531e381d773dfea110cf9
blue.pen5805 <blue.pen5805@fastmail.com>
2025-02-11 08:47:10 +0000

    fix: adjust cooldown parameters for improved rate limiting efficiency

--- aiuser/aiuser.py
+++ aiuser/aiuser.py
@@ -55,9 +55,9 @@ class AIUser(
         self.ignore_regex: dict[int, re.Pattern] = {}
         self.override_prompt_start_time: dict[int, datetime] = {}
         self.cached_messages: Cache[int, MessageEntry] = Cache(limit=100)
-        self.cooldown1 = commands.CooldownMapping.from_cooldown(2, 15.0, commands.BucketType.channel)
-        self.cooldown2 = commands.CooldownMapping.from_cooldown(3, 60.0, commands.BucketType.channel)
-        self.cooldown3 = commands.CooldownMapping.from_cooldown(10, 180.0, commands.BucketType.channel)
+        self.cooldown1 = commands.CooldownMapping.from_cooldown(1, 10.0, commands.BucketType.channel)
+        self.cooldown2 = commands.CooldownMapping.from_cooldown(2, 30.0, commands.BucketType.channel)
+        self.cooldown3 = commands.CooldownMapping.from_cooldown(3, 60.0, commands.BucketType.channel)
 
         default_global = {
             "custom_openai_endpoint": None,
